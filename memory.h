8ee46d823dc32917e58145b8a53dd4d5c526599e
Ricardo Soares <romao.soares@gmail.com>
2024-06-02 02:27:25 -0300

    add hastable

--- memory.h
+++ memory.h
@@ -19,7 +19,7 @@
 #define FREE_ARRAY(type, pointer, oldCount)\
         reallocate(pointer, sizeof(type) * (oldCount), 0)
 
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
-void *reallocate(void* pointer, size_t oldSize, size_t newSize);
 void freeObjects();
 #endif
---
+++ table.c
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "value.h"
+
+#define TABLE_MAX_LOAD 0.75
+
+void initable(Table* table) {
+    table->count = 0;
+    table->capacity = 0;
+    table->entities = NULL;
+}
+
+void freeTable(Table* table) {
+    FREE_ARRAY(Entry, table->entities, table->capacity);
+    initTable(table);
+}
+
+static Entry* findEntry(Entry* entities, int capacity, ObjString* key) {
+    uint32_t index = key->hash % capacity;
+    Entry* tombstone = NULL;
+
+    for (;;) {
+        Entry* entry = &entities[index];
+        if (entry->key == key || entry->key == NULL) {
+            if (IS_NIL(entry->key)) {
+                return tombstone != NULL ? tombstone : entry;
+            } else {
+                if (tombstone == NULL) tombstone = entry;
+                if (entry->key == key) return entry;
+            }
+        }
+        index = (index + 1) % capacity;
+    } 
+}
+
+static void adjustTable(Table* table, int capacity) {
+    
+    Entry* entities = ALLOCATE(Entry, capacity);
+
+    for (int i = 0; i < capacity; i++) {
+        entities[i].key = NULL;
+        entities[i].value = NIL_VAL;
+    }
+
+    table->count = 0;
+
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entities[i];
+        if (entry->key == NULL) continue;
+
+        Entry* dest = findEntry(entities, capacity, entry->key);
+        dest->key = entry->key;
+        dest->value = entry->value;
+        table->count++;
+    }
+
+    FREE_ARRAY(Entry, table->entities, table->capacity);
+
+    table->entities = entities;
+    table->capacity = capacity;
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+    
+    if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+        int capacity = GROW_CAPACITY(table->capacity);
+        adjustTable(table, capacity);
+    }
+
+    Entry* entry = findEntry(table->entities, table->capacity, key);
+    bool isNewKey = entry->key == NULL;
+
+    if (isNewKey && IS_NIL(entry->value)) table->count++;
+    
+    entry->key = key;
+    entry->value = value;
+    return true;
+}
+
+bool tableDelete(Table* table, ObjString* key) {
+    if (table->count == 0) return false;
+
+    // Find the entry.
+    Entry* entry = findEntry(table->entities, table->capacity, key);
+    if (entry->key == NULL) return false;
+    // Place a tombstone in the entry.
+    entry->key = NULL;
+    entry->value = BOOL_VAL(true);
+
+    return true;
+}
+
+void tableAddAll(Table* from, Table* to) {
+    for (int i = 0; i < from->capacity; i++) {
+        Entry* entry = &from->entities[i];
+        if (entry->key != NULL) {
+            tableSet(to, entry->key, entry->value);
+        }
+    }
+}
+
+bool tableGet(Table* table, ObjString* key, Value* value) {
+    if (table->count == 0) return false;
+
+    Entry* entry = findEntry(table->entities, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
