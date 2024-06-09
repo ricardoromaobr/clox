29e4a08257b606637667ec37579b705a7407f344
Ricardo Soares <romao.soares@gmail.com>
2024-06-09 17:55:52 -0300

    Struct to locals variables

--- compiler.c
+++ compiler.c
@@ -15,6 +15,17 @@ typedef struct
     bool panicMode;
 } Parser;
 
+typedef struct {
+    Token name; 
+    int depth
+} Local;
+
+typedef struct {
+    Local locals[UINT8_COUNT];
+    int localCount; 
+    int scopeDepth;
+} Compiler;
+
 typedef enum
 {
     PREC_NONE,
