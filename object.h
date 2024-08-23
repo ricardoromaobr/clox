219e9ce3524792409adf6307a75557fc0cd795ce
Ricardo Soares <romao.soares@gmail.com>
2024-08-23 09:14:46 -0300

    change type ObjClass to ObjClass*

--- object.h
+++ object.h
@@ -10,7 +10,7 @@
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value,OBJ_NATIVE)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
-#define AS_CLASS(value) ((ObjClass) AS_OBJ(value))
+#define AS_CLASS(value) ((ObjClass*) AS_OBJ(value))
 #define AS_CLOSURE(value) ((ObjClosure*) AS_OBJ(value))
 #define AS_FUNCTION(value) ((ObjFunction*) AS_OBJ(value))
 #define AS_NATIVE(value) (((ObjNative*)AS_OBJ(value))->function)
